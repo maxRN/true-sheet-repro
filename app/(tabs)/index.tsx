d4b12051c0ee9d447523abbd2c26c0b2abfe0ae5
Max Große <max.grosse@zepta.io>
2026-02-23 19:41:01 +0100

    style: format with prettier + upgrade deps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,124 +1,144 @@
-import { Button, View, StyleSheet, ScrollView, Text, RefreshControl } from 'react-native';
-import { TrueSheet } from "@lodev09/react-native-true-sheet"
-import { useRef, useState } from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {
+  Button,
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  RefreshControl,
+} from "react-native";
+import { TrueSheet } from "@lodev09/react-native-true-sheet";
+import { useRef, useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-    const insets = useSafeAreaInsets();
-    const sheet = useRef<TrueSheet>(null)
-    const [showDetails, setShowDetails] = useState(false);
+  const insets = useSafeAreaInsets();
+  const sheet = useRef<TrueSheet>(null);
+  const [showDetails, setShowDetails] = useState(false);
 
-    // Present the sheet ✅
-    const present = async () => {
-        await sheet.current?.present()
-        console.log('horray! sheet has been presented 💩')
-    }
+  // Present the sheet ✅
+  const present = async () => {
+    await sheet.current?.present();
+    console.log("horray! sheet has been presented 💩");
+  };
 
-    // Dismiss the sheet ✅
-    const dismiss = async () => {
-        await sheet.current?.dismiss()
-        console.log('Bye bye 👋')
-    }
+  // Dismiss the sheet ✅
+  const dismiss = async () => {
+    await sheet.current?.dismiss();
+    console.log("Bye bye 👋");
+  };
 
-    return (
-        <View style={{ paddingBottom: insets.bottom, paddingTop: insets.top }}>
-            <Button onPress={present} title="Present" />
-            <TrueSheet
-                ref={sheet}
-                detents={[0.3, 1]}
-                cornerRadius={24}
-                dismissible={false}
-                dimmed={false}
-                header={<View style={{ padding: 30 }} />}
-                scrollable
-            >
-                <Button onPress={dismiss} title="Dismiss" />
-                <Button onPress={() => setShowDetails(!showDetails)} title={showDetails ? "Go back" : "Go to details view"} />
-                {!showDetails ?
-                    (
-                        <ScrollView nestedScrollEnabled={false} refreshControl={<RefreshControl refreshing={false} onRefresh={() => console.log("refreshing...")} />}>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                            <Text>Some long text</Text>
-                        </ScrollView>) : <Text>test</Text>}
-            </TrueSheet>
-        </View>
-    )
+  return (
+    <View style={{ paddingBottom: insets.bottom, paddingTop: insets.top }}>
+      <Button onPress={present} title="Present" />
+      <TrueSheet
+        ref={sheet}
+        detents={[0.3, 1]}
+        cornerRadius={24}
+        dismissible={false}
+        dimmed={false}
+        header={<View style={{ padding: 30 }} />}
+        scrollable
+      >
+        <Button onPress={dismiss} title="Dismiss" />
+        <Button
+          onPress={() => setShowDetails(!showDetails)}
+          title={showDetails ? "Go back" : "Go to details view"}
+        />
+        {!showDetails ? (
+          <ScrollView
+            nestedScrollEnabled={false}
+            refreshControl={
+              <RefreshControl
+                refreshing={false}
+                onRefresh={() => console.log("refreshing...")}
+              />
+            }
+          >
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+            <Text>Some long text</Text>
+          </ScrollView>
+        ) : (
+          <Text>test</Text>
+        )}
+      </TrueSheet>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-    },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { ReanimatedTrueSheetProvider } from '@lodev09/react-native-true-sheet/reanimated'
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { ReanimatedTrueSheetProvider } from "@lodev09/react-native-true-sheet/reanimated";
 
-import 'react-native-reanimated';
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-    anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    return (
-        <ReanimatedTrueSheetProvider>
-            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-                <Stack>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-                </Stack>
-                <StatusBar style="auto" />
-            </ThemeProvider>
-        </ReanimatedTrueSheetProvider>
-    );
+  return (
+    <ReanimatedTrueSheetProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </ReanimatedTrueSheetProvider>
+  );
 }
