838b8e0e003ca420918a14332a30df3401251967
Max Große <max.grosse@zepta.io>
2026-02-23 19:41:53 +0100

    remove dead code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,4 @@
-import {
-  Button,
-  View,
-  StyleSheet,
-  ScrollView,
-  Text,
-  RefreshControl,
-} from "react-native";
+import { Button, View, ScrollView, Text, RefreshControl } from "react-native";
 import { TrueSheet } from "@lodev09/react-native-true-sheet";
 import { useRef, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -123,22 +116,3 @@ export default function HomeScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
