8d5c8f4bf03048dd6d97b29308af9a2457e1a66e
Max Große <max.grosse@zepta.io>
2026-02-24 16:06:04 +0100

    feat: add iOS reanimated reproduction

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: "Explore",
+          title: "Reanimated Opacity",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="paperplane.fill" color={color} />
           ),
